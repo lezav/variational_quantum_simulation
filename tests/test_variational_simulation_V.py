2f11c4119674927fc83f40b9d23445c99252e477
admunozmoller <andmunoz2016@udec.cl>
2022-02-22 17:28:53 -0300

    added function and test for the V matrix

---
+++ tests/test_variational_simulation_V.py
@@ -0,0 +1,18 @@
+import numpy as np
+from core.variational_simulation import A, V
+
+n_qubits = 2
+J = 1/2
+B = 1/2
+fs = [[-1j*J], [-1j*B, -1j*B]]
+params = np.array([1.0, 1.0])
+ops = [["ZZ"], ["XI", "IX"]]
+
+hs = [-2.0*J, -B, -B]
+opsH = ["ZZ", "XI", "IX"]
+
+# a = A(params, fs, ops, n_qubits)
+v = V(params, fs, hs, ops, opsH, n_qubits)
+
+# print(a)
+print(v)
