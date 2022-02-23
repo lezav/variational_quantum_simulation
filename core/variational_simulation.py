050ddaeea3edb4fdca8bef1e7564b19af70f6d9d
Jorge Gidi <jorgegidi@gmail.com>
2022-02-23 02:42:01 -0300

    add V-related (to be corrected) and updated P to parse_gate

--- core/variational_simulation.py
+++ core/variational_simulation.py
@@ -6,7 +6,7 @@ from qiskit.circuit.library.standard_gates import *
 from qiskit.quantum_info.operators import Operator, Pauli
 from qiskit.extensions import HamiltonianGate
 from qiskit.quantum_info import Statevector
-from core.utils import P
+from core.utils import parse_gate
 
 def initial_state(n_qubits):
     qr_data = QuantumRegister(n_qubits, "data") # data register
@@ -124,6 +124,104 @@ def A_kqij(params, fs, ops, n_qubits, k, q, i, j, shots=8192):
     Re_0U0 = (counts.get("0", 0) - counts.get("1", 0))/shots
     return a_kiqj*Re_0U0
 
+def V(params, fs, hs, ops, opsH, n_qubits):
+    """
+    Calculate the matrix A
+    """
+    N = params.shape[0]
+    v = np.zeros(N)
+    for k in range(N):
+        v[k] = V_k(params, fs, hs, ops, opsH, n_qubits, k)
+    return v
+
+
+def V_k(params, fs, hs, ops, opsH, n_qubits, k):
+    """
+    Calculate a term V_k that appear in equation (13) of the paper.
+    Args:
+        params:  array (N,). theta_k parameters in the paper.
+        fs: list (N). List that contain N lists. The inner lists contain the
+            complex coefficients f_ki.
+        ops: list (N). List that contain N lists. The inner lists contain
+            the operators sigma_ki.
+        k: int. params for which we want to calculate V_k.
+    Returns:
+        v_k: float. Eq. (13).
+    """
+
+    # select the elements from the lists
+    n_k = len(fs[k])
+    n_i = len(hs)
+    v_k = 0
+    for i in range(n_k):
+        for j in range(n_i):
+            v_k += V_kij(params, fs, hs, ops, opsH, n_qubits, k, i, j)
+    return v_k
+
+
+def V_kij(params, fs, hs, ops, opsH, n_qubits, k, i, j, shots=8192):
+    """
+    Calculate V_kij = f*_ki h_j <0|R^dagg_ki sigma_J R|0> that appear in Eq. (13).
+    Args:
+        params:  array (N,). theta_k parameters in the paper.
+        fs: list (N). List that contain N lists. The inner lists contain the
+            complex coefficients f_ki.
+        hs: list (N). List that contain N lists. The inner lists contain the
+            coefficients h_i.
+        ops: list (N). List that contain N lists. The inner lists contain
+            the operators sigma_ki.
+        k, i, j: int. params for which we want to calculate V_kij.
+    Returns:
+        v_kij: float. Dot product of derivatives given by Eq. (10).
+    """
+    # We create the circuit with n_qubits plus an ancilla.
+    qr_ancilla = QuantumRegister(1, "ancilla") # ancilla register
+    qr_data = QuantumRegister(n_qubits, "data") # data register
+    cr = ClassicalRegister(1, "cr") # classical register
+    qc = QuantumCircuit(qr_ancilla, qr_data, cr)
+    # preparate the ancilla in the state |0> + e^(theta)|1>
+    N = params.shape[0]
+    a_v_kij = 2*np.abs(np.conjugate(fs[k][i])*hs[j])
+    theta_kij = np.angle(np.conjugate(fs[k][i])*hs[j])
+    qc.append(initial_state(n_qubits), qr_data[:])
+    qc.h(qr_ancilla)
+    qc.p(theta_kij, qr_ancilla)
+    qc.barrier()
+    # Now we want to construct R_ki
+    # apply R_1 ...R_k-1 gates
+    for m in range(k):
+        R = R_k(params[m], fs[m], ops[m], n_qubits)
+        qc.append(R, qr_data[:])
+    qc.barrier()
+    # apply the controlled operation for sigma_ki
+    qc.x(qr_ancilla)
+    controlled_Uk = string2U(ops[k][i], n_qubits).control(num_ctrl_qubits=1)
+    qc.append(controlled_Uk, qr_ancilla[:] + qr_data[:])
+    qc.barrier()
+    # apply R_k...R_N gates
+    for m in range(k, N):
+        R = R_k(params[m], fs[m], ops[m], n_qubits)
+        qc.append(R, qr_data[:])
+    qc.barrier()
+    qc.x(qr_ancilla)
+    # apply the controlled operation for sigma_j
+    controlled_U = string2U(opsH[j], n_qubits).control(num_ctrl_qubits=1)
+    qc.append(controlled_U, qr_ancilla[:] + qr_data[:])
+    qc.barrier()
+    # measure in the X basis with a number of shots
+    qc.h(qr_ancilla)
+    qc.measure(qr_ancilla, cr)
+    # print(qc.draw())
+    simulator = Aer.get_backend('aer_simulator')
+    # simulator = Aer.get_backend('statevector_simulator')
+    qc = transpile(qc, simulator)
+    # print(circ.draw())
+    result = simulator.run(qc, shots=shots).result()
+    counts = result.get_counts(qc)
+    #calculate a Re(e^(theta) <0|U|0>)
+    Re_0U0 = (counts.get("0", 0) - counts.get("1", 0))/shots
+    return a_v_kij*Re_0U0
+
 
 def controlled_gates(ops_ki, k, i, n_qubits):
     qr_data = QuantumRegister(n_qubits, "data") # data register
@@ -143,8 +241,8 @@ def string2U(op, n_qubits):
     qr_data = QuantumRegister(n_qubits, "data") # data register
     qc = QuantumCircuit(qr_data)
     for m in range(n_qubits):
-        qc.unitary(Operator(P(op[m])), qr_data[m])
-    # qc.unitary(Operator(P(op)), qr_data[::])
+        qc.unitary(Operator(parse_gate(op[m])), qr_data[m])
+    # qc.unitary(Operator(parse_gate(op)), qr_data[::])
     return qc.to_gate(label=op)
 
 
@@ -160,8 +258,8 @@ def R_k(params_k, fs_k, ops_k, n_qubits):
     """
 
     n_k = len(ops_k)
-    Ops_k = fs_k[0]*Operator(P(ops_k[0]))
+    Ops_k = fs_k[0]*Operator(parse_gate(ops_k[0]))
     for j in range(1, n_k):
-        Ops_k += fs_k[j]*Operator(P(ops_k[j]))
+        Ops_k += fs_k[j]*Operator(parse_gate(ops_k[j]))
 
     return HamiltonianGate(1j*Ops_k , params_k, label="+".join(ops_k))
