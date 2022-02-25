75b4024caf47bfd19f198db139a891f14ed47d17
Mariana Navarro <42687846+MarianaNvrr@users.noreply.github.com>
2022-02-24 21:40:56 -0300

    changes in the time evolution

--- core/schrodinger.py
+++ core/schrodinger.py
@@ -7,14 +7,14 @@ def time_evolution(opsH, hs, dt, Nt, img=True):
     n_ops = len(opsH)
     n_qubits = len(opsH[0])
     Hamiltonian = hs[0]*parse_gate(opsH[0])
-    U = np.zeros((2**n_qubits, 2**n_qubits, Nt), dtype="complex")
+    U = np.zeros((Nt,2**n_qubits, 2**n_qubits), dtype="complex")
     for j in range(1, n_ops):
         Hamiltonian += hs[j]*parse_gate(opsH[j])
     for n in range(Nt):
         if img==True:
-            U[:, :, n] = la.expm(-1j*dt*n*Hamiltonian)
+            U[n, :, :] = la.expm(-1j*dt*n*Hamiltonian)
         elif img==False:
-            U[:, :, n] = la.expm(dt*n*Hamiltonian)
+            U[n, :, :] = la.expm(dt*n*Hamiltonian)
     return U
 
 
@@ -23,8 +23,14 @@ def state_evoluted(initial_state, opsH, hs, dt, Nt, img=True):
     state_evoluted = np.zeros((Nt, 2**n_qubits), dtype="complex")
     U = time_evolution(opsH, hs, dt, Nt, img=True)
     for n in range(Nt):
-        state_evoluted[n,:] = np.dot(U[:, :, n], initial_state)
+        state_evoluted[n,:] = np.dot(U[n, :, :], initial_state)
     normalize_state_evoluted = state_evoluted/np.linalg.norm(state_evoluted,
                                                              axis=1,
                                                              keepdims=True)
     return normalize_state_evoluted
+
+def energy_evolution(State, Hamiltonian, Nt):
+    energy = []
+    for n in range(Nt):
+        energy = np.conjugate(State[n].T) @ Hamiltonian @ State[n]
+    return energy
