15ffe34ab558e6ac4cecb43d5d3a6fb5bf7f2918
lezav <41238801+lezav@users.noreply.github.com>
2022-02-25 17:18:18 -0300

    Update tutorial and tests deleted

--- tests/test_utils_test_P.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from core.utils import P
-import scipy
-from qiskit.quantum_info.operators import Operator, Pauli
-
-ops = [["ZZI", "IZZ", "ZIZ"], ["XII", "IXI", "IIX"]]
-Operator(Pauli("XII")) - P("XII")
