b3684a9c928c6cb39620b9c80f4e540de783a5f9
Jorge Gidi <jorgegidi@gmail.com>
2022-02-24 22:18:05 -0300

    revisit integrators

--- core/integrators.py
+++ core/integrators.py
@@ -4,24 +4,24 @@ import numpy as np
 
 # 1st-order integrator on dt
 def euler(ode, x0, dt, Nt):
-    acc = np.empty((Nt, len(x0)), dtype=type(x0[0]))
+    acc = np.empty_like(x0, shape=(Nt, len(x0)))
     acc[0, :] = x0
-    for t in range(1, Nt):
-        acc[t, :] = acc[t-1, :] + dt*ode(acc[t-1, :], dt*(t-1) )
+    for n in range(Nt-1):
+        acc[n+1, :] = acc[n] + dt*ode(acc[n], n*dt)
 
     return acc
 
 # 4-th order runge-kutta
 def rk4(ode, x0, dt, Nt):
-    acc = np.empty((Nt, len(x0)), dtype=type(x0[0]))
+    acc = np.empty_like(x0, shape=(Nt, len(x0)))
     acc[0, :] = x0
-    for n in range(1, Nt):
-        tn = (n-1)*dt
-        yn = acc[n-1, :]
+    for n in range(Nt-1):
+        tn = n*dt
+        yn = acc[n, :]
         k1 = dt * ode(yn, tn)
         k2 = dt * ode(yn + k1/2, tn + dt/2)
         k3 = dt * ode(yn + k2/2, tn + dt/2)
         k4 = dt * ode(yn + k3,   tn + dt)
-        acc[n, :] = yn + (k1 + 2*k2 + 2*k3 + k4)/6
+        acc[n+1, :] = yn + (k1 + 2*k2 + 2*k3 + k4)/6
 
     return acc
